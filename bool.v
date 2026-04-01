fed0884891c8f5a345d04a21c16c8e0dfc9709f8
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-04-01 18:31:13 +0100

    fun proofs

--- bool.v
+++ bool.v
@@ -25,19 +25,34 @@ Module Bool.
 
     Notation "x && y" := (and x y).
     Notation "x || y" := (or x y).
+
+    Example or_true_false:
+        or true false = true.
+    Proof. reflexivity. Qed.
+
+    Example or_true_true:
+        true || false = true.
+    Proof. reflexivity. Qed.
+
+    Definition nand (b1 b2 : bool) : bool :=
+        not (b1 && b2).
+
+    Example nand_true_false:
+        nand true false = true.
+    Proof. simpl. reflexivity. Qed.
+
+    Theorem and_commutitive : forall b c : bool, 
+        b && c = c && b.
+    Proof. 
+        intros proof_b.
+        intros proof_c.
+        destruct proof_b.
+            + destruct proof_c.
+                reflexivity.
+                reflexivity.
+            + destruct proof_c.
+                reflexivity.
+                reflexivity.
+    Qed.
+
 End Bool.
-
-Example or_true_false:
-    or true false = true.
-Proof. reflexivity. Qed.
-
-Example or_true_true:
-    true || false = true.
-Proof. reflexivity. Qed.
-
-Definition nand (b1 b2 : bool) : bool :=
-    not (b1 && b2).
-
-Example nand_true_false:
-    nand true false = true.
-Proof. simpl. reflexivity. Qed.
---
+++ nat.v
@@ -0,0 +1,21 @@
+Theorem plus_id : forall n m : nat,
+    n = m -> n + n = n + m. 
+Proof. 
+    intros proof_n.
+    intros proof_m. 
+    intros n_equal_m.
+    rewrite n_equal_m.
+    reflexivity.
+Qed.
+
+Theorem identity : forall n : nat, 
+    n + 0 = n.
+Proof. 
+    intros proof_n.
+    induction proof_n.
+        + compute. 
+          reflexivity.
+        + simpl. 
+          rewrite IHproof_n. 
+          reflexivity.
+Qed.
