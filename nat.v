f4c4d1ee50cf75bf1809f65dcb6124bdb9cf4902
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-04-04 23:02:50 +0100

    using modules

--- nat.v
+++ nat.v
@@ -1,3 +1,5 @@
+Require Import Arith.
+
 Theorem plus_id : forall n m : nat,
     n = m -> n + n = n + m.
 Proof.
@@ -30,15 +32,29 @@ Proof.
     apply identity.
 Qed.
 
-Theorem sum_minus : forall n m : nat, 
+Theorem sub_ge_0 : forall n m : nat, 
     n >= m -> n - m >= 0.
 Proof.
     intros proof_n. 
     intros proof_m.
     intros n_bigger_equal_m.
     induction proof_n.
-        + simpl. apply le_n. 
+        + simpl. apply le_0_n. 
         + simpl. destruct proof_m.
             - assumption.
             - apply le_0_n.
 Qed.
+
+Theorem ge_n_m: forall k n m : nat,
+    n <= m -> k * n <= k * m. 
+Proof.
+    intros proof_k.
+    intros proof_n.
+    intros proof_m.
+    intros n_le_m.
+    induction proof_k.
+        + simpl. apply le_n.
+        + simpl. apply Nat.add_le_mono.
+            - assumption.
+            - assumption.
+Qed.            
